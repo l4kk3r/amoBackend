e633cd258fb74e7539a63c434965fb236a8024dd
Bob <l4kk3r@yandex.ru>
2021-06-08 20:48:08 +0300

    added env link

--- database/database.js
+++ database/database.js
@@ -1,18 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('hospaltrip', 'postgres', '123', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: 0,
-  logging: false,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-});
+const db = new Sequelize(process.env.DATABASE_URL)
 
 db.sync({ alter: true }).then(() => {
   console.log('Database syncronized');
