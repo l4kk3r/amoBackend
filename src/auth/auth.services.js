4e7254b03667f3397bc8a469ad73daafe9552e69
Bob <l4kk3r@yandex.ru>
2021-06-08 20:57:11 +0300

    trying

--- src/auth/auth.services.js
+++ src/auth/auth.services.js
@@ -8,10 +8,6 @@ exports.register = async (req, res) => {
     const user = req.body
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
     user.password = hashedPassword
-    try {
-        await User.create(user)
-    } catch {
-        return res.status(409).json({ message: 'User with this email already exists' })
-    }
+    await User.create(user)
     res.json({ message: 'User created!' })
 }
