2c428694b2a60f93f0cad0720149acd1c71e8820
Bob <l4kk3r@yandex.ru>
2021-06-08 21:28:30 +0300

    fixed ssl problem

--- database/database.js
+++ database/database.js
@@ -1,6 +1,20 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL)
+db = null;
+db = new Sequelize('postgres://ubjldtflxufkxe:5c19cbe147b67e9d15179c6ef92f2477bfa7821bd1760c806b7c805452d18446@ec2-52-213-119-221.eu-west-1.compute.amazonaws.com:5432/dac92pegjkq5ls',
+          {
+            dialect: "postgres",
+            protocol: "postgres",
+            port: 5432,
+            host: "hospaltrip.herokuapp.com",
+            logging: false,
+            dialectOptions: {
+              ssl: {
+                require: true,
+                rejectUnauthorized: false
+              }
+            }
+});
 
 db.sync({ alter: true }).then(() => {
   console.log('Database syncronized');
