f795cf0c7fe94a227597416eb8a72039b9e1c8f3
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2021-06-09 09:29:56 +0300

    Update auth.routes.js

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -4,6 +4,65 @@ const router = express.Router()
 const authServices = require('./auth.services')
 const userMiddlewares = require('@src/user/user.middlewares')
 
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * security:
+ *   - bearerAuth: []   
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Request_Auth_Login:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: User email
+ *         password:
+ *           type: string
+ *           description: User password
+ *       example:
+ *         email: johndoe@yandex.ru
+ *         password: JohnPassword123
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: The authentication managing API
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Login to site
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Request_Auth_Login'
+ *     responses:
+ *       200:
+ *         description: Data for logging into site
+ */
+
 router.post('/register', userMiddlewares.register, authServices.register)
 router.post('/login', userMiddlewares.login, authServices.login)
 
