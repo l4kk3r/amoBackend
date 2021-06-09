abc46da2b883720cf84af386cd52d5e02d1d7056
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2021-06-09 20:01:47 +0300

    Update auth.services.js

--- src/auth/auth.services.js
+++ src/auth/auth.services.js
@@ -8,23 +8,23 @@ const saltRounds = 10
 
 exports.register = async (req, res) => {
     try {
-        const user = req.body
+        const userData = req.body
         
         const userWithSameEmail = await User.findOne({
-                email: user.email
+                email: userData.email
         })
         if (userWithSameEmail) {
             return res.status(409).json({ message: 'User with this email already exists' })
         } else {
-            const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
-            user.password = hashedPassword
-            await User.create(user)
+            const hashedPassword = await bcrypt.hash(userData.password, saltRounds)
+            userData.password = hashedPassword
+            await User.create(userData)
         }
 
-        const userFromDB = await User.findOne({
-            email: user.email
+        const user = await User.findOne({
+            email: userData.email
         })
-        const jwtToken = jwt.sign({ userFromDB }, process.env.JWT_SECRET)
+        const jwtToken = jwt.sign({ user }, process.env.JWT_SECRET)
         
         res.json({ message: 'User created!', jwtToken })
     } catch {
@@ -38,7 +38,7 @@ exports.login = async (req, res) => {
 
         const user = await User.findOne({
            email
-        })
+        }).select("+password")
         if (! user) return res.status(422).json({ message: 'User with this email does not exist'})
 
         const hashedPassword = user.password
