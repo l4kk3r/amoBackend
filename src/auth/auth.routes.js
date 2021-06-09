991174ca6eaf47b3453dedba4f7132b4ceb10a33
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2021-06-09 09:50:20 +0300

    Update auth.routes.js

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -39,6 +39,37 @@ const userMiddlewares = require('@src/user/user.middlewares')
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     Request_Auth_Register:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *         - confirmPassword
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: User name
+ *         email:
+ *           type: string
+ *           description: User email
+ *         password:
+ *           type: string
+ *           description: User password
+ *         confirmPassword:
+ *           type: string
+ *           description: Password confirmation
+ *       example:
+ *         name: John
+ *         email: johndoe@yandex.ru
+ *         password: JohnPassword123
+ *         confirmPassword: JohnPassword123
+ */
+
+/**
+ * @swagger
  * tags:
  *   name: Auth
  *   description: The authentication managing API
@@ -58,6 +89,25 @@ const userMiddlewares = require('@src/user/user.middlewares')
  *         application/json:
  *           schema:
  *             $ref: '#/components/schemas/Request_Auth_Login'
+ *     responses:
+ *       200:
+ *         description: Data for logging into site
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Register user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Request_Auth_Register'
  *     responses:
  *       200:
  *         description: Data for logging into site
