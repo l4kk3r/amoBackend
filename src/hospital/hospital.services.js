98845c60764d925096dee7e7f495b13bd1153530
Bob <l4kk3r@yandex.ru>
2021-06-15 15:05:45 +0300

    fixed query thing

--- src/hospital/hospital.services.js
+++ src/hospital/hospital.services.js
@@ -3,27 +3,30 @@ const mongoose = require('mongoose')
 const Hospital = mongoose.model('Hospital')
 const Country = mongoose.model('Country')
 
+const modelIdByNameTranslit = async (Model, nameTranslit) => {
+    const object = await Model.findOne({nameTranslit})
+    if (!object) return [null, 'City not found']
+    return [object._id, null]
+}
 const getFiltersFromQuery = async (query) => {
-    const queryData = {
-        country: query.country
-    }
     const filters = {}
-    console.log(queryData.country.toLowerCase())
-    const  countryFromDB = queryData.country ? await Country.findOne({nameTranslit: queryData.country.toLowerCase()}) : undefined
-    filters.country_id = countryFromDB ? countryFromDB._id : null
-    Object.keys(filters).forEach(key => filters[key] === undefined && delete filters[key]) // удаление полей с undefined значением
-    return filters
+    if (query.country) {
+        const [country_id, error] = await modelIdByNameTranslit(Country, query.country)
+        if (error) return [null, error]
+        filters.country = country_id
+    }
+    return [filters, null]
 }
 
 exports.getAll = async (req, res) => {
-    const filters = await getFiltersFromQuery(req.query)
-    console.log(filters)
+    const [filters, error] = await getFiltersFromQuery(req.query)
+    if (error) return res.status(404).json({ message: error })
 
-    hospitals = await Hospital.find({ 'country': filters.country_id })
+    hospitals = await Hospital.find(filters)
     if (!hospitals.length > 0) return res.status(404).json({ message: 'Hospitals not found' })
 
     res.json({ hospitals })
-}
+} 
 
 exports.getOne = async (req, res) => {
     hospital = await Hospital.findOne({ url: req.params.url }).populate('doctors country')
